3c7ab256e35a6821eb6124f136c3098b4f2ae792
wuanzhuan <1691111985@qq.com>
2023-12-24 15:51:48 +0800

    optimize

--- src/event_trace/mod.rs
+++ src/event_trace/mod.rs
@@ -3,7 +3,7 @@ use std::{
     rc::Rc,
     sync::{
         mpsc::{self, RecvTimeoutError},
-        Arc, Mutex,
+        Arc, Mutex, MutexGuard,
     },
     thread,
     time::Duration,
@@ -78,6 +78,189 @@ impl Controller {
         cxt
     }
 
+    pub fn start(fn_event_callback: impl Fn(EventRecordDecoded) + Send + 'static, fn_completion: impl FnOnce(Result<()>) + Send + 'static) -> Result<()> {
+        let context_arc = CONTEXT.clone();
+        let mut context_mg = context_arc
+            .try_lock()
+            .map_err(|_| ERROR_LOCK_VIOLATION.to_hresult())?;
+        let mut h_trace = CONTROLTRACE_HANDLE::default();
+        let session_name: &U16CStr = if context_mg.is_win8_or_greater {
+            SESSION_NAME_SYSMON
+        } else {
+            SESSION_NAME_NT
+        };
+        let mut properties_buf = make_properties(context_mg.is_win8_or_greater, session_name);
+
+        let r = loop {
+            loop {
+                let r = unsafe{
+                    StartTraceW(
+                        &mut h_trace,
+                        session_name.as_pcwstr(),
+                        &mut properties_buf.0,
+                    )
+                };
+                match r {
+                    Ok(_) => {
+                        context_mg.h_trace_session = h_trace;
+                        break Ok(());
+                    }
+                    Err(e) => {
+                        if e.code() == ERROR_ALREADY_EXISTS.to_hresult() {
+                            if let Err(e) = unsafe{
+                                ControlTraceW(
+                                    CONTROLTRACE_HANDLE::default(),
+                                    session_name.as_pcwstr(),
+                                    &mut properties_buf.0,
+                                    EVENT_TRACE_CONTROL_STOP,
+                                )
+                            } {
+                                error!("The {session_name:#?} is already exist. And failed to stop: {:#?}", e);
+                                break Err(e);
+                            };
+                            warn!("The {session_name:#?} is already exist. and stop before restart");
+                            continue;
+                        }
+                        error!("Failed to StartTraceW: {:#?}", e);
+                        break Err(e);
+                    }
+                }
+            }?;
+
+            if let Err(e) = context_mg.update_config() {
+                break Err(e);
+            }
+    
+            context_mg.event_record_callback = Some(Rc::new(fn_event_callback));
+            let mut trace_log = EVENT_TRACE_LOGFILEW {
+                Context: &mut *context_mg as *mut Controller as *mut ffi::c_void,
+                LoggerName: PWSTR::from_raw(session_name.as_ptr() as *mut u16),
+                Anonymous1: EVENT_TRACE_LOGFILEW_0 {
+                    ProcessTraceMode: PROCESS_TRACE_MODE_EVENT_RECORD
+                        | PROCESS_TRACE_MODE_REAL_TIME,
+                },
+                Anonymous2: EVENT_TRACE_LOGFILEW_1 {
+                    EventRecordCallback: Some(Controller::callback),
+                },
+                ..Default::default()
+            };
+            let h_consumer = unsafe{ OpenTraceW(&mut trace_log) };
+            if INVALID_PROCESSTRACE_HANDLE == h_consumer.Value {
+                context_mg.event_record_callback = None;
+                let e = Err(Error::from_win32());
+                error!("Failed to OpenTraceW: {:#?}", e);
+                break e;
+            }
+            context_mg.h_trace_consumer = h_consumer;
+    
+            let (tx, rx) = mpsc::channel::<Error>();
+            let h_thread = thread::spawn(move || {
+                let ft_now = unsafe{ GetSystemTimeAsFileTime() };
+                let r_pt = unsafe{ ProcessTrace(&[h_consumer], Some(&ft_now), None) };
+                if let Err(e) = r_pt.clone() {
+                    error!("Failed to ProcessTrace: {}", e);
+                    let r_send = tx.send(e);
+                    if r_send.is_ok() {
+                        return;
+                    }
+                }
+                let context_arc = CONTEXT.clone();
+                let mut context_mg = context_arc.lock().unwrap();
+                context_mg.h_consumer_thread = None;
+                fn_completion(r_pt);
+            });
+            let r_recv = rx.recv_timeout(Duration::from_millis(200));
+            match r_recv {
+                Err(e) => {
+                    if e == RecvTimeoutError::Timeout {
+                        context_mg.h_consumer_thread = Some(h_thread);
+                        break Ok(());
+                    }
+                    error!("Failed to recv_timeout {}", e);
+                    context_mg.h_consumer_thread = None;
+                    break Err(E_FAIL.into());
+                },
+                Ok(e) => {
+                    error!("{}", e);
+                    context_mg.h_consumer_thread = None;
+                    break Err(e);
+                }
+            }
+        };
+        if r.is_err() {
+            let _ = Self::stop(Some(context_mg));
+        }
+        r
+    }
+
+    pub fn stop(mg: Option<MutexGuard<Controller>>) -> Result<()> {
+        let context_arc = CONTEXT.clone();
+        let mut context_mg = mg.unwrap_or(context_arc
+            .try_lock()
+            .map_err(|_| ERROR_LOCK_VIOLATION.to_hresult())?);
+
+        if 0 != context_mg.h_trace_session.Value {
+            let session_name: &U16CStr = if context_mg.is_win8_or_greater {
+                SESSION_NAME_SYSMON
+            } else {
+                SESSION_NAME_NT
+            };
+            let mut properties_buf = make_properties(context_mg.is_win8_or_greater, session_name);
+            let error = unsafe{
+                ControlTraceW(
+                    context_mg.h_trace_session,
+                    session_name.as_pcwstr(),
+                    &mut properties_buf.0,
+                    EVENT_TRACE_CONTROL_STOP,
+                )
+            };
+            context_mg.h_trace_session.Value = 0;
+            if let Err(e) = error {
+                error!("failed to ControlTraceW {}", e);
+            }
+        }
+
+        if INVALID_PROCESSTRACE_HANDLE != context_mg.h_trace_consumer.Value {
+            let error = unsafe{ CloseTrace(context_mg.h_trace_consumer) };
+            context_mg.h_trace_consumer.Value = INVALID_PROCESSTRACE_HANDLE;
+            if let Err(e) = error {
+                if ERROR_CTX_CLOSE_PENDING.to_hresult() != e.code() {
+                    error!("failed to CloseTrace {}", e);
+                }
+            }
+        }
+        if context_mg.h_consumer_thread.is_some() {
+            let h = context_mg.h_consumer_thread.take().unwrap();
+            mem::drop(context_mg);
+            let _ = h.join();
+        }
+        Ok(())
+    }
+
+    pub fn set_config_enables(index_major: usize, index_minor: Option<usize>, checked: bool) {
+        let context_arc = CONTEXT.clone();
+        if let Ok(mut context_mg) = context_arc.lock() {
+            let mut is_change = false;
+            if let Some(index) = index_minor {
+                if context_mg.config.events_enables[index_major].minors[index] != checked {
+                    if !context_mg.config.events_enables[index_major].major {
+                        context_mg.config.events_enables[index_major].major = true;
+                    }
+                    is_change = true;
+                    context_mg.config.events_enables[index_major].minors[index] = checked;
+                }
+            } else {
+                if context_mg.config.events_enables[index_major].major != checked {
+                    is_change = true;
+                    context_mg.config.events_enables[index_major].major = checked;
+                }
+            }
+            if is_change && 0 != context_mg.h_trace_session.Value {
+                let _ = context_mg.update_config();
+            }
+        };
+    }
+
     unsafe extern "system" fn callback(eventrecord: *mut EVENT_RECORD) {
         let r = event_decoder::Decoder::new(mem::transmute(eventrecord));
         match r {
@@ -105,189 +288,29 @@ impl Controller {
         }
     }
 
-    pub fn start(fn_event_callback: impl Fn(EventRecordDecoded) + Send + 'static, fn_completion: impl FnOnce(Result<()>) + Send + 'static) -> Result<()> {
-        let context_arc = CONTEXT.clone();
-        let mut context_mg = context_arc
-            .try_lock()
-            .map_err(|_| ERROR_LOCK_VIOLATION.to_hresult())?;
-        let mut h_trace = CONTROLTRACE_HANDLE::default();
-        let session_name: &U16CStr = if context_mg.is_win8_or_greater {
-            SESSION_NAME_SYSMON
-        } else {
-            SESSION_NAME_NT
-        };
-        let mut properties_buf = make_properties(context_mg.is_win8_or_greater, session_name);
-
-        let mut error: Result<()>;
-        loop {
-            error = unsafe{
-                StartTraceW(
-                    &mut h_trace,
-                    session_name.as_pcwstr(),
-                    &mut properties_buf.0,
-                )
-            };
-            match error {
-                Ok(_) => {
-                    context_mg.h_trace_session = h_trace;
-                    break;
-                }
-                Err(e) => {
-                    if e.code() == ERROR_ALREADY_EXISTS.to_hresult() {
-                        let error = unsafe{
-                            ControlTraceW(
-                                CONTROLTRACE_HANDLE::default(),
-                                session_name.as_pcwstr(),
-                                &mut properties_buf.0,
-                                EVENT_TRACE_CONTROL_STOP,
-                            )
-                        };
-                        if error.is_ok() {
-                            warn!("The {session_name:#?} is already exist. and stop before restart");
-                            continue;
-                        }
-                        error!(
-                            "The {session_name:#?} is already exist. And failed to stop: {:#?}",
-                            error
-                        );
-                        return error;
-                    }
-                    error!("Failed to StartTraceW: {:#?}", e);
-                    return Err(e);
-                }
-            }
-        }
-
-        context_mg.update_config()?;
-
-        context_mg.event_record_callback = Some(Rc::new(fn_event_callback));
-        let mut trace_log = EVENT_TRACE_LOGFILEW {
-            Context: &mut *context_mg as *mut Controller as *mut ffi::c_void,
-            LoggerName: PWSTR::from_raw(session_name.as_ptr() as *mut u16),
-            Anonymous1: EVENT_TRACE_LOGFILEW_0 {
-                ProcessTraceMode: PROCESS_TRACE_MODE_EVENT_RECORD
-                    | PROCESS_TRACE_MODE_REAL_TIME,
-            },
-            Anonymous2: EVENT_TRACE_LOGFILEW_1 {
-                EventRecordCallback: Some(Controller::callback),
-            },
-            ..Default::default()
-        };
-        let h_consumer = unsafe{ OpenTraceW(&mut trace_log) };
-        if INVALID_PROCESSTRACE_HANDLE == h_consumer.Value {
-            context_mg.event_record_callback = None;
-            return Err(Error::from_win32());
-        }
-        context_mg.h_trace_consumer = h_consumer;
-
-        let (tx, rx) = mpsc::channel::<Error>();
-        let h_thread = thread::spawn(move || {
-            let ft_now = unsafe{ GetSystemTimeAsFileTime() };
-            let r_pt = unsafe{ ProcessTrace(&[h_consumer], Some(&ft_now), None) };
-            if let Err(e) = r_pt.clone() {
-                error!("Failed to ProcessTrace: {}", e);
-                let r_send = tx.send(e);
-                if r_send.is_ok() {
-                    return;
-                }
-            }
-            let context_arc = CONTEXT.clone();
-            let mut context_mg = context_arc.lock().unwrap();
-            context_mg.h_consumer_thread = None;
-            fn_completion(r_pt);
-        });
-        let r_recv = rx.recv_timeout(Duration::from_millis(200));
-        match r_recv {
-            Err(e) => {
-                if e == RecvTimeoutError::Timeout {
-                    context_mg.h_consumer_thread = Some(h_thread);
-                    return Ok(());
-                }
-                error!("{}", e);
-                context_mg.h_consumer_thread = None;
-                return Err(E_FAIL.into());
-            },
-            Ok(e) => {
-                error!("{}", e);
-                context_mg.h_consumer_thread = None;
-                return Err(e);
-            }
-        }
-    }
-
-    pub fn stop() -> Result<()> {
-        let context_arc = CONTEXT.clone();
-        let mut context_mg = context_arc
-            .try_lock()
-            .map_err(|_| ERROR_LOCK_VIOLATION.to_hresult())?;
-
-            if 0 != context_mg.h_trace_session.Value {
-                let session_name: &U16CStr = if context_mg.is_win8_or_greater {
-                    SESSION_NAME_SYSMON
-                } else {
-                    SESSION_NAME_NT
-                };
-                let mut properties_buf = make_properties(context_mg.is_win8_or_greater, session_name);
-                let error = unsafe{
-                    ControlTraceW(
-                        context_mg.h_trace_session,
-                        session_name.as_pcwstr(),
-                        &mut properties_buf.0,
-                        EVENT_TRACE_CONTROL_STOP,
-                    )
-                };
-                context_mg.h_trace_session.Value = 0;
-                if let Err(e) = error {
-                    error!("failed to ControlTraceW {}", e);
-                }
-            }
-
-            if INVALID_PROCESSTRACE_HANDLE != context_mg.h_trace_consumer.Value {
-                let error = unsafe{ CloseTrace(context_mg.h_trace_consumer) };
-                context_mg.h_trace_consumer.Value = INVALID_PROCESSTRACE_HANDLE;
-                if let Err(e) = error {
-                    if ERROR_CTX_CLOSE_PENDING.to_hresult() != e.code() {
-                        error!("failed to CloseTrace {}", e);
-                    }
-                }
-            }
-        if context_mg.h_consumer_thread.is_some() {
-            let h = context_mg.h_consumer_thread.take().unwrap();
-            mem::drop(context_mg);
-            let _ = h.join();
-        }
-        Ok(())
-    }
-
-    pub fn set_config_enables(index_major: usize, index_minor: Option<usize>, checked: bool) {
-        let context_arc = CONTEXT.clone();
-        if let Ok(mut context_mg) = context_arc.lock() {
-            if let Some(index) = index_minor {
-                context_mg.config.events_enables[index_major].minors[index] = checked;
-                let _ = context_mg.update_config();
-            } else {
-                context_mg.config.events_enables[index_major].major = checked;
-            }
-        };
-    }
-
     fn update_config(&self) -> Result<()> {
-        let gm = self.config.get_group_mask(); 
-        unsafe{
+        let gm = self.config.get_group_mask();
+        if let Err(e) = unsafe {
             TraceSetInformation(
                 self.h_trace_session,
                 TraceSystemTraceEnableFlagsInfo,
                 ptr::addr_of!(gm.masks) as *const ffi::c_void,
                 std::mem::size_of_val(&gm.masks) as u32,
             )
-        }?;
+        } {
+            error!("Failed to TraceSetInformation TraceSystemTraceEnableFlagsInfo: {}", e);
+            return Err(e);
+        }
         let vec_event_id = self.config.get_classic_event_id_vec();
-        unsafe{ TraceSetInformation(
+        if let Err(e) = unsafe{ TraceSetInformation(
             self.h_trace_session,
             TraceStackTracingInfo,
             vec_event_id.as_ptr() as *const ffi::c_void,
             vec_event_id.len() as u32,
-        ) }?;
+        )} {
+            error!("Failed to TraceSetInformation TraceStackTracingInfo: {}", e);
+            return Err(e);
+        }
         Ok(())
     }
 }
