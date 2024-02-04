52d1c29bf54e558b785dab6106193372ee920668
wuanzhuan <1691111985@qq.com>
2024-02-05 00:08:50 +0800

    add requireAdministrator

--- build.rs
+++ build.rs
@@ -3,4 +3,11 @@
 
 fn main() {
     slint_build::compile("src/ui/main.slint").unwrap();
+
+    let pkg_name = std::env::var("CARGO_PKG_NAME").unwrap();
+    #[cfg(windows)]
+    {
+        println!("{}", format!("cargo:rustc-link-arg-bin={pkg_name}=/MANIFEST:EMBED"));
+        println!("{}", format!("cargo:rustc-link-arg-bin={pkg_name}=/MANIFESTUAC:level=\'requireAdministrator\'"));
+    }
 }
