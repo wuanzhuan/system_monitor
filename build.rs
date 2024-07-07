7c676a6fb99a689b55075c4e502eac35f1e84b71
wuanzhuan <1691111985@qq.com>
2024-07-07 21:38:03 +0800

    set fluent-dark and fix miss vcruntime140.dll

--- build.rs
+++ build.rs
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 fn main() {
-    slint_build::compile("src/ui/main.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new().with_style("fluent-dark".into());
+    slint_build::compile_with_config("src/ui/main.slint", config).unwrap();
 
     let pkg_name = std::env::var("CARGO_PKG_NAME").unwrap();
     #[cfg(windows)]
@@ -17,5 +18,6 @@ fn main() {
                 "cargo:rustc-link-arg-bin={pkg_name}=/MANIFESTUAC:level=\'requireAdministrator\'"
             )
         );
+        static_vcruntime::metabuild();
     }
 }
